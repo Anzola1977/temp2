e7e35372dd48f37a2404dea511f61557459bfb6d
Andrew Zola <and.zola.77@gmail.com>
2022-10-01 22:09:03 +0200

    First commit

---
+++ src/TrafficLight.java
@@ -0,0 +1,27 @@
+public class TrafficLight {
+    public static void main(String[] args) {
+        String colorTrafficLight1 = "green";
+        String colorTrafficLight2 = "red";
+        String colorTrafficLight3 = "yellow";
+        System.out.println(lighting(colorTrafficLight1)); //+ " " + lighting(colorTrafficLight2) + " " + lighting(colorTrafficLight3));
+    }
+
+    public static String lighting(String colorTrafficLight) {
+        String result = "";
+
+        switch (colorTrafficLight) {
+            case "green":
+                result = "You can drive!";
+                break;
+            case "red":
+                result = "You must stop!";
+                break;
+            case "yellow":
+                result = "You must wait!";
+                break;
+            default:
+                result = "traffic light not working";
+        }
+        return result;
+    }
+}
